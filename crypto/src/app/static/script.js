c0aafe95280d0da0b2e04a3fddb30264844faf30
pkemkes <pkemkes@gmail.com>
2023-03-11 15:57:58 +0100

    Finalize crypto challenge level 0

--- crypto/src/app/static/script.js
+++ crypto/src/app/static/script.js
@@ -1,6 +1,9 @@
 var textEnc = document.getElementById("text-enc");
 var textDec = document.getElementById("text-dec");
-var keyInput = document.getElementById("key");
+var keyBox = document.getElementById("key");
+var arrowUp = document.getElementById("arrow-up");
+var arrowDown = document.getElementById("arrow-down");
+var toLetters = document.getElementsByClassName("to");
 
 Number.prototype.mod = function(n) {
     "use strict";
@@ -8,11 +11,12 @@ Number.prototype.mod = function(n) {
 };
 
 var DICT = {};
+var M = 26;
 
 function parseKey(keyStr) {
     if (typeof keyStr != "string") return false;
     let key = parseInt(keyStr);
-    if (isNaN(keyStr) || isNaN(key) || key < 0 || key >= 26) {
+    if (isNaN(keyStr) || isNaN(key) || key < 0 || key >= M) {
         throw "invalid key " + keyStr;
     }
     return key;
@@ -20,7 +24,7 @@ function parseKey(keyStr) {
 
 function moveBy(charCode, key, first, last) {
     let base = first.charCodeAt();
-    let m = last.charCodeAt() - base;
+    let m = last.charCodeAt() - base + 1;
     return ((charCode - base - key).mod(m)) + base;
 }
 
@@ -31,7 +35,7 @@ function addToCesarDict(key, first, last) {
 }
 
 function updateCaesarDict() {
-    let key = parseKey(keyInput.value);
+    let key = parseKey(keyBox.innerHTML);
     addToCesarDict(key, "a", "z");
     addToCesarDict(key, "A", "Z");
 }
@@ -52,6 +56,30 @@ function decrypt() {
     textDec.innerHTML = dec;
 }
 
-keyInput.addEventListener("input", decrypt);
+function changeOrder(key) {
+    for (let i = 0; i < toLetters.length; i++) {
+        let accessor = (i + key).mod(M);
+        toLetters[accessor].style.order = i;
+    }
+}
+
+function changeKey(inc) {
+    try {
+        var key = parseKey(keyBox.innerHTML)
+    }
+    catch(err) {
+        console.error(err);
+        return;
+    }
+    key = (inc ? key + 1 : key - 1).mod(M);
+    keyBox.innerHTML = key;
+    changeOrder(key);
+    decrypt();
+}
+
+arrowUp.addEventListener("click", () => changeKey(true));
+arrowDown.addEventListener("click", () => changeKey(false));
 
 decrypt();
+
+document.getElementById("js-warning").style.display = "none";
--- crypto/src/app/views.py
+++ crypto/src/app/views.py
@@ -1,10 +1,29 @@
 import os
 from app import app
 from flask import render_template, Response
+from random import randint
 
-FLAG = os.getenv("FLAG")
+TEXT_TO_DECRYPT = os.getenv("TEXT_TO_DECRYPT")
+
+
+def move_by(letter_ord: int, key: int, start: int, end: int) -> int:
+    return ((letter_ord - start + key) % (end - start + 1)) + start
+
+
+def enc(text: str, key: int) -> str:
+    char_dict = {}
+    for start, end in [("a", "z"), ("A", "Z")]:
+        char_dict.update({chr(c): chr(move_by(c, key, ord(start), ord(end)))
+                          for c in range(ord(start), ord(end)+1)})
+    return "".join(char_dict.get(c, c) for c in text)
+
+
+def get_enc(text_to_dec: str) -> str:
+    key = randint(5, 25)
+    return enc(text_to_dec, key)
 
 
 @app.route("/", methods=["GET"])
 def index() -> Response:
-    return render_template("index.html")
+    enc_text = get_enc(TEXT_TO_DECRYPT)
+    return render_template("index.html", enc=enc_text)
