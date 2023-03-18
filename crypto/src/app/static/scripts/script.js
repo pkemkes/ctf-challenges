459972ede864d7681deb0678a897b9ebb4385504
pkemkes <pkemkes@gmail.com>
2023-03-18 09:08:06 +0100

    Fix clock number padding

--- crypto/src/app/static/scripts/script.js
+++ crypto/src/app/static/scripts/script.js
@@ -2,7 +2,7 @@ var clock = document.getElementById("time");
 
 function UpdateClock() {
     var now = new Date();
-    var time = now.getHours() + ":" + now.getMinutes();
+    var time = now.getHours() + ":" + now.getMinutes().toString().padStart(2, "0");
     let clockTime = clock.innerHTML;
     if (clockTime != time) {
         clock.innerHTML = time;
